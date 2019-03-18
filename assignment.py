3d522e41bfc7511b8d6e865cc1c51f5b5a3686d6
UbeMaster <jglem@shaw.ca>
2019-03-18 16:51:38 -0600

    fixed comments plus additional testing

--- assignment.py
+++ assignment.py
@@ -2,8 +2,11 @@
 #C-I-S-C-O assignment
 import sys, json
 
+#Initialize pathCheck boolean, which determine if the filename 
+#in the "ph" field is checked with the "nm" field
 pathCheck = True
 
+#Check the command line to see if path check should be turned off
 try:
 	if sys.argv[2] == "pathCheckOff":
 		pathCheck = False
@@ -18,7 +21,7 @@ try:
 except IOError:
 	print ("Could not open file ", sys.argv[1], " for reading.")
 except IndexError:
-	print ("No argument was given for filename. Try again with filename added. \nEx. \"python3 assignment.py log.json\" or \"python3 assignment.py <path to file>\"")
+	print ("No argument was given for filename. Try again with filename added. \nEx. \"python3 assignment.py log.json\" or \"python3 assignment.py <path to file from directory holding assignment.py>\"")
 	sys.exit()
 
 #Create a dictionary for extensions
@@ -33,30 +36,27 @@ for line in fileHandle:
 	validLine = True
 	
 	#Load json data
-	#If line is not in JSON format, catch the exception from loading and flag the line as invalid
+	#If line is not in proper JSON format, catch the exception from loading and flag the line as invalid
 	try:
 		data = json.loads(line)
 	except:
+		print("couldn't load json properly")
 		validLine = False
-
+		
+	#########Check to see the given JSON data is valid#########
 	#If the JSON data was loaded, make sure each field name is found in the JSON data
 	if validLine is not False:
+		
+		#For each expected field name
 		for fName in fieldNames:
-			#If the entry name is not found, the line is not valid, break
+			
+			#1) Check if the field name is found in the loaded JSON data
 			if fName not in data:		
 				validLine = False
 				print("Missing: "+fName)
 				break
-
-			#Make sure none of the entries have types that are not int nor string. 
-			#This makes sure there are no nested json entries inside of the fields
-			#Ex. {"address": {"street":123, cat:"meow"}}
-			if not isinstance(data[fName],int) and not isinstance(data[fName],str):
-				validLine = False
-				print("Field is not str or int. It is"+str(type(data[fName])))
-				break
 				
-			#Check that for each entry, the given field is the right type
+			#2) Check that for each field, the given field is the right type
 			if (fName == "ts" or fName == "pt" or fName == "dp") and not isinstance(data[fName], int):
 				validLine = False
 				print(fName+" has field that is not int")
@@ -66,36 +66,50 @@ for line in fileHandle:
 				print(fName+" has field that is not str")
 				break
 			
-			if pathCheck:
-				if fName == "ph":
+			#3) If path check is turned on and the current fName is "ph"
+			#check the filename in "ph" against the "nm" field
+			if pathCheck and fName == "ph":
+				
+					#Find the index where the last '/' is
 					slashIndex = data[fName].rfind("/")
 					pathFileName = ""
-					if slashIndex > -1:
+					
+					#If a slash was found,
+					#create substring consisting of everything after that last '/'
+					if slashIndex > -1:						
 						pathFileName = data[fName][slashIndex+1:]
+						
+					#else, the path is just the filename
 					else:
 						pathFileName = data[fName]
-				
+					
+					#If the names do not match, the line is not valid
 					if pathFileName != data["nm"]:
+						print("ph and nm don't match")
 						validLine = False
 						break
 			
-			#If fName is "dp", check if the dp value is valid
+			#4) If fName is "dp", check if the dp value is valid,
+			#get the dp value
 			if fName == "dp":
-				#Get the dpValue
+				
 				dpValue = data[fName]
 				print("dpValue: "+str(dpValue))
 				
 				#If the dpValue is not 1,2, or 3, the line is invalid
 				if int(dpValue) < 1 or int(dpValue) > 3:
 					validLine = False
+					print("dp value invalid")
 					break
 				
-	#If line is not valid, continue to next line in JSON file
+	#If line is not valid, continue to the next line in the JSON file
 	if validLine == False:
 		print("invalid line: "+line)
 		continue
 	
+	#########Record unique filenames into a set of nested dictionaries#########
 
+	
 	#Retrieve filename from nm field
 	fileName = data["nm"]
 
@@ -106,59 +120,55 @@ for line in fileHandle:
 	
 	#If no dot is found, this is a file without an extension
 	if (dotIndex == -1):
-		#If there is not a "No extension" entry in the extensions dictionary
-		if "No extension" not in extensions:
-			#Make a "No extension" entry and set it to an empty dictionary
+		#If there is not a "No extension" entry in the extensions dictionary,
+		#make a "No extension" entry and set it to an empty dictionary,
+		#and inside that nested dictionary, record an entry for the filename and set it to a value
+		if "No extension" not in extensions:			
 			extensions["No extension"] = {}
-			#Inside that nested entry, record an entry for the filename
-			extensions["No extension"][fileName] = 1
-		else:
-			#If there is an entry for "No extension" in extensions, see if fileName is found in the "No extension" dictionary
-			if fileName not in extensions["No extension"]:
-				#If not found in the "No extension" dictionary, add the fileName as a new entry
-				extensions["No extension"][fileName] = 1
-			else:
-				#fileName entry is found in the "No extension" dictionary
-				print("Double found! " + fileName)
 
-	#If a dot is found in the fileName
+		#If fileName is bit found in the "No extension" dictionary,
+		#add a new entry in the "No extensions" dictionary for fileName
+		if fileName not in extensions["No extension"]:
+			extensions["No extension"][fileName] = 1
+		#else: fileName entry is found in the "No extension" dictionary
+		else:				
+			print("Double found! " + fileName)
+
+	#If a dot is found in the fileName, this is a filename with an extension
 	else:
 		#Separate the extension name from the just the filename
 		extName = fileName[dotIndex+1:]
 		fileName = fileName[0:dotIndex]
 		#print(fileName)
 		#print(extName)
-		#If the extName is not an entry in the extensions dictionary
-		if extName not in extensions:
-			#Make an entry for extName and set it to an empty dictionary
-			extensions[extName] = {}
-			#Add the fileName as an entry in the new empty dictionary for extName
-			extensions[extName][fileName]=1
-		
-		#If the extName has an entry in the extensions dictionary
-		else:
-			#If the fileName is not found in the nested extName dictionary
-			if fileName not in extensions[extName]:
-				#Add fileName as a new entry in the nested extName dictionary
-				extensions[extName][fileName]=1
-			
-			else:
-				#fileName entry is found in the nested extName dictionary
-				print("Double found! " + fileName +"."+extName)
-				continue
+		#If the extName is not an entry in the extensions dictionary,
+		#make an entry for extName and set it to an empty dictionary
+		#and Add the fileName as an entry in the new empty dictionary for extName.
+		if extName not in extensions:			
+			extensions[extName] = {}			
 
+		#If the fileName is not found in the nested extName dictionary,
+		#add fileName as a new entry in the nested extName dictionary
+		if fileName not in extensions[extName]:				
+			extensions[extName][fileName]=1
+			print("aiya")
+		#else: fileName entry is found in the nested extName dictionary			
+		else:				
+			print("Double found! " + fileName +"."+extName)
+			continue
+
+
+#########Print results and tally number of unique filenames#########
 sum=0
-#Print results
 print("Results:")
 if len(extensions) > 0:
 	for element in extensions:
-
 		print(element+": "+str(len(extensions[element])))
 		sum+= len(extensions[element])
 else:
 	print("No valid lines found in file")
 
-print("total: "+str(sum))
+print("total number of unique filenames: "+str(sum))
 		
 
 	
